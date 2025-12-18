653d591ccf0577d76cf7a28f83ec2ddd6ec44289
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-18 16:02:59 +0530

    Add DTO

---
+++ AdmitResultAPI/Model/DTOs/DummyAdmitCardDto.cs
@@ -0,0 +1,49 @@
+﻿namespace AdmitResultAPI.Model.DTOs
+{
+    public class DummyAdmitCardDto
+    {
+        public int StudentID { get; set; }
+
+        public string? StudentName { get; set; }
+
+        public int? CollegeId { get; set; }
+
+        public int? FacultyId { get; set; }
+
+        public string? CollegeCode { get; set; }
+
+        public int? ExamTypeId { get; set; }
+
+        public string? FatherName { get; set; }
+
+        public string? MotherName { get; set; }
+
+        public string? DOB { get; set; }   // VARCHAR(10) from SQL
+
+        public string? CategoryName { get; set; }
+
+        public string? Faculty { get; set; }
+
+        public string? College { get; set; }
+
+        public string? OfssReferenceNo { get; set; }
+
+        public bool? FormDownloaded { get; set; }
+
+        public bool? IsRegCardUploaded { get; set; }
+
+        public string? RegCardPath { get; set; }
+
+        public string? PracticalExamCenterName { get; set; }
+
+        public string? TheoryExamCenterName { get; set; }
+
+        public string? RegistrationNo { get; set; }
+
+        public string? RollCode { get; set; }
+
+        public string? RollNumber { get; set; }
+
+        public string? GenderName { get; set; }
+    }
+}
