5a56da7bf3d453449c9dc077367efefa1284ae96
Anuja Dubey <anuja.dubey@ssbi.in>
2025-12-18 16:16:50 +0530

    created request DTO

---
+++ AdmitResultAPI/Model/DTOs/DummyAdmitCardRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AdmitResultAPI.Model.DTOs
+{
+    public class DummyAdmitCardRequestDto
+    {
+        public string? RegistrationNo { get; set; }
+        public string? RollCode { get; set; }
+        public string? RollNo { get; set; }
+
+        public int? IsPractical { get; set; }
+        public int? IsTheory { get; set; }
+    }
+}
