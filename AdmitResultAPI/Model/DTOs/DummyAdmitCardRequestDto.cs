d829dfd0315dd0512926b11e1f00f374c61bc628
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-20 17:50:55 +0530

    fOR pHOTO CHANGES

--- AdmitResultAPI/Model/DTOs/DummyAdmitCardRequestDto.cs
+++ AdmitResultAPI/Model/DTOs/DummyAdmitCardRequestDto.cs
@@ -3,10 +3,10 @@
     public class DummyAdmitCardRequestDto
     {
         public string? RegistrationNo { get; set; }
-        public string? RollCode { get; set; }
+        public int? RollCode { get; set; }
         public string? RollNo { get; set; }
 
-        public int? IsPractical { get; set; }
-        public int? IsTheory { get; set; }
+        //public int? IsPractical { get; set; }
+        //public int? IsTheory { get; set; }
     }
 }
